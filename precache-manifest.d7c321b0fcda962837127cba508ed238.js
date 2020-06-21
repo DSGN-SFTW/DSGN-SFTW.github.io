661c762ecad17389a54b3f615be557abf21f8333
Arthur Carneiro <arthur.carneirontk@gmail.com>
2020-06-21 15:10:38 -0300

    Updates

---
+++ precache-manifest.d7c321b0fcda962837127cba508ed238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3d8755abd9e449a7bf49da2ef12d75",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "e8edbbfce9b12e19452c",
+    "url": "/frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "370c9fd060cfe3a4ac85",
+    "url": "/frontend/static/js/2.6d20342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.6d20342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8edbbfce9b12e19452c",
+    "url": "/frontend/static/js/main.4531c84d.chunk.js"
+  },
+  {
+    "revision": "bb9cf7b820440dcc0565",
+    "url": "/frontend/static/js/runtime-main.e2bed40b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
